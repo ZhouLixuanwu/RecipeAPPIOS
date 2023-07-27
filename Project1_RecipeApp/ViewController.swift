4817957bee046ac6bc163509c99ab6417dfd6f3a
ZhouLixuanwu <xavierzhouli@gmail.com>
2023-07-27 10:34:48 -0700

    Initial Commit

---
+++ Project1_RecipeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1_RecipeApp
+//
+//  Created by LoveIrelia on 7/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
